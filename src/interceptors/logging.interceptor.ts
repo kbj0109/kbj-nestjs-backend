ad84d3324d170dd8558a490185f3a6473df447cd
kbj0109 <kbj0109@gmail.com>
2024-04-11 15:39:22 +0900

    Error 로그 기록 추가

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -1,6 +1,7 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, tap } from 'rxjs';
+import chalk from 'chalk';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -9,24 +10,48 @@ export class LoggingInterceptor implements NestInterceptor {
 
     const httpContext = context.switchToHttp();
     const request = httpContext.getRequest() as Request;
-    const response = httpContext.getResponse() as Response;
 
     // const apiAddress = request.protocol + '://' + request.get('host') + request.originalUrl; // ex) API 주소
     const requestType = context.getType();
+    const httpMethod = request.method;
 
     return next.handle().pipe(
       tap(() => {
         const spentTime = new Date().getTime() - startTime.getTime();
-        const httpMethod = request.method;
 
-        console.log(
-          `[${requestType}]`,
-          `${httpMethod}`,
-          `${request.originalUrl}`,
-          response.statusCode,
-          spentTime,
-          'ms',
-        );
+        const response = httpContext.getResponse() as Response;
+
+        let statusCode = response.statusCode.toString();
+        if (statusCode.startsWith('2')) {
+          statusCode = chalk.green(statusCode);
+        }
+        if (statusCode.startsWith('4')) {
+          statusCode = chalk.yellow(statusCode);
+        }
+        if (statusCode.startsWith('5')) {
+          statusCode = chalk.red(statusCode);
+        }
+
+        console.log(`[${requestType}]`, `${statusCode}`, `${httpMethod}`, `${request.originalUrl}`, spentTime, 'ms');
+      }),
+
+      catchError((err: any) => {
+        const spentTime = new Date().getTime() - startTime.getTime();
+
+        let statusCode = (err.status || 500).toString();
+        if (statusCode.startsWith('2')) {
+          statusCode = chalk.green(statusCode);
+        }
+        if (statusCode.startsWith('4')) {
+          statusCode = chalk.yellow(statusCode);
+        }
+        if (statusCode.startsWith('5')) {
+          statusCode = chalk.red(statusCode);
+        }
+
+        console.log(`[${requestType}]`, `${statusCode}`, `${httpMethod}`, `${request.originalUrl}`, spentTime, 'ms');
+
+        throw err;
       }),
     );
   }
